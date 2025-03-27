0740bbb4c8cea2622339985979f32a328afa528b
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2025-03-27 18:31:47 +0530

    changed shell.nix to be a file instead of a symlink

--- shell.nix
+++ shell.nix
@@ -1,1 +1,19 @@
-/home/mathew/dotfiles/.config/Environments/C.shell.nix
+# shell.nix for C/C++ Environment
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShell {
+  name = "C";
+  packages = with pkgs; [
+    libgcc
+    clang-tools
+
+    binutils
+    glibc
+
+  ];
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    pkgs.stdenv.cc.cc
+  ];
+}
