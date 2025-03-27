ee9ea228633fc4d52f4a0782bac54ebc58cc5388
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2025-03-27 18:45:33 +0530

    minor changes

--- extract.sh
+++ extract.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -o main.zip https://github.com/mastermach50/OS-Lab-Programs/archive/refs/heads/main.zip
+curl -o main.zip -L https://github.com/mastermach50/OS-Lab-Programs/archive/refs/heads/main.zip
 unzip main.zip -d .
 rm main.zip .gitignore .envrc README.md shell.nix
